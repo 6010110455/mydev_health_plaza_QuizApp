b454b89f833484190f93e09a17a428b0c2b0cf6c
6010110455 <6510120049@email.psu.ac.th>
2024-05-19 15:09:02 +0700

    re structure project

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../components/HomeScreen';
+
 
 const Stack = createStackNavigator();
 const AppNavigator = () => {
